23fad6210419e262b4e44f08757e2a798fef54f9
Ali Imran <aliimran20004@gmail.com>
2025-05-03 20:18:30 +0500

    Create chat card component with basic states

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ChatCard from './ChatCard'
 
 function ContactList() {
 
     return (
         <>
-            <div id="sidebar" className=" flex flex-col items-center justify-end w-auto ml-4 text-wrap p-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quis reprehenderit eligendi tenetur exercitationem earum ipsa sequi ab assumenda. Impedit praesentium eius neque earum illum eum hic id itaque officia?
+            <div id="sidebar" className=" flex flex-col items-center justify-end w-auto ml-1 text-wrap p-1">
+                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quis reprehenderit eligendi tenetur exercitationem earum ipsa sequi ab assumenda. Impedit praesentium eius neque earum illum eum hic id itaque officia?
                 Vero exercitationem ducimus, aliquam, dicta enim nostrum nihil culpa et minima, totam neque! Provident, magni eveniet! Labore earum enim tenetur rerum. Ea quisquam aut dolores suscipit sit voluptates quaerat non?
                 Nobis molestias tenetur vero nulla deleniti eos, ut omnis quo! Vero illo dolorum, deserunt provident, nesciunt accusamus velit at asperiores aliquid ab soluta unde suscipit excepturi accusantium esse libero eum?
                 Itaque tenetur facere molestiae magnam laborum vero illo reiciendis aliquid? Consequatur ab, voluptatum ullam nihil doloremque illum reiciendis quidem voluptas amet corporis ipsam quasi ea, magnam eligendi dolore itaque quia.
@@ -14,7 +15,54 @@ function ContactList() {
                 Debitis accusantium sint maxime, nulla voluptate, quidem quod, eaque libero porro exercitationem in. Ullam itaque officiis, id obcaecati eaque consectetur impedit non vero, eum ut unde incidunt. Eos, amet quas!
                 Provident, quaerat quidem quasi autem minus deserunt sint, dolorum eum enim consectetur libero repudiandae sed praesentium corporis numquam facilis, cum maxime! Labore nostrum praesentium repellat fugiat consequatur iusto necessitatibus sapiente.
                 Corrupti dignissimos vel voluptatibus possimus dolor, dicta perspiciatis, odio unde veniam, nisi repellat. Qui omnis eveniet optio facilis delectus sit quo perferendis possimus, atque quas placeat voluptas cumque tempora dolorem.
-                Dolorum recusandae, vitae iusto culpa, incidunt dignissimos provident deserunt optio officia nihil modi nesciunt quasi magnam amet aperiam expedita minima possimus id exercitationem! Earum laborum veritatis doloremque laboriosam maiores aperiam?
+                Dolorum recusandae, vitae iusto culpa, incidunt dignissimos provident deserunt optio officia nihil modi nesciunt quasi magnam amet aperiam expedita minima possimus id exercitationem! Earum laborum veritatis doloremque laboriosam maiores aperiam? */}
+
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
             </div>
         </>
     )
