0e027ac67f17f5a43d62ac7b757078e6af924eee
Ali Imran <aliimran20004@gmail.com>
2025-05-08 18:09:47 +0500

    Merge remote changes from main

--- src/components/VoiceMessage.jsx
+++ src/components/VoiceMessage.jsx
@@ -7,8 +7,8 @@ const VoiceMessage = ({ message }) => {
     <div className={`message ${from === 'me' ? 'me' : 'other'}`}>
       {from === 'other' && <img src={avatar} alt={senderName} className="avatar" />}
       <div className="voice-message">
-        <div className="play-pause-icon">▶️</div> {/* Placeholder for play/pause icon */}
-        <div className="duration">{content}</div> {/* Display duration of the voice message */}
+        <div className="play-pause-icon">▶️</div>
+        <div className="duration">{content}</div>
       </div>
       <div className="message-time">{time}</div>
     </div>
