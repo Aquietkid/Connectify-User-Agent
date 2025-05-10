b233fa2dc00b95101069147ac9dce6556f866bf7
MuhammadUmar7831 <mu8494759@gmail.com>
2025-05-11 02:50:22 +0500

    fixing merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import Home from './components/Home'
 import _404Page from './components/_404Page'
 import PrivateRoute from './components/PrivateRoute'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import MainWindow from './components/chat-area/MainWindow'
-import NavSidebar from './components/NavSidebar'
-import Sidebar from './components/Sidebar'
-import ProfileComponent from './components/ProfileComponent'
-import Auth from './components/Auth'
-import ChatAreaProvider from './context/ChatAreaContext'
 
 
 function App() {
@@ -18,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/signin' element={<Auth />} />
         <Route path='/signup' element={<Auth />} />
-        <Route path='/home' element={
+        <Route path='/' element={
           <PrivateRoute>
             <Home />
           </PrivateRoute>
